d946c041478906d2fb9d79116075826a5472a49a
ramish-khan204 <159920981+ramish-khan204@users.noreply.github.com>
2026-01-05 13:13:54 -0800

    Time: 0 ms (100%), Space: 42.8 MB (20.48%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    // Initialize stack
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    // Push element onto stack
+    public void push(int x) {
+        q2.offer(x);
+
+        // Move all elements from q1 to q2
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+
+        // Swap q1 and q2
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    // Removes the top element
+    public int pop() {
+        return q1.poll();
+    }
+
+    // Get top element
+    public int top() {
+        return q1.peek();
+    }
+
+    // Check if stack is empty
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
